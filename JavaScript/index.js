e14411aca2d0fc6248cf517dae64763665cc26dd
Yolanda Lopez <ylg.10502@gmail.com>
2023-05-31 13:36:23 -0600

    FizzBuzz with array

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -45,3 +45,20 @@ function isLeap(year) {
     return "Not leap year.";
   }
 }
+
+/** FizzBuzz Exercise with arrays */
+var output = [];
+var count = 1;
+function FizzBuzz() {
+  if (count % 3 === 0 && count % 5 === 0) {
+    output.push("FizzBuzz");
+  } else if (count % 3 === 0) {
+    output.push("Fizz");
+  } else if (count % 5 === 0) {
+    output.push("Buzz");
+  } else {
+    output.push(count);
+  }
+  count++;
+  console.log(output);
+}
