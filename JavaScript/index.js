128be48f32219831e795abc9957a9ebadcd3b7d9
Yolanda Lopez <ylg.10502@gmail.com>
2023-05-31 12:47:09 -0600

    Love calculator

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -27,21 +27,3 @@ function loveCalculator() {
     loveScore = Math.floor(loveScore) + 1;
     alert("Your love score is " + loveScore + "%");
   }
-
-/** Leap Year Challenge Exercise */
-
-function isLeap(year) {
-  if (year % 4 === 0) {
-    if (year % 100 === 0) {
-      if (year % 400 === 0) {
-        return "Leap year.";
-      } else {
-        return "Not leap year.";
-      }
-    } else {
-      return "Leap year.";
-    }
-  } else {
-    return "Not leap year.";
-  }
-}
