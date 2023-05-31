92b550798e7a22e016022bb6c9d19e60f2440aee
Yolanda Lopez <ylg.10502@gmail.com>
2023-05-31 12:46:27 -0600

    BMI calculator

---
+++ JavaScript/index.js
@@ -0,0 +1,47 @@
+/* JavaScript exercises from Section 15*/
+
+/** BMI Calculator */
+
+function bmiCalculator(weight, height) {
+  const bmi = weight / (height * height);
+  var interpretation;
+
+  if (bmi < 18.5) {
+    interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+  } else if (bmi > 18.5 && bmi < 24.9) {
+    interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+  } else if (bmi < 29.5) {
+    interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+  }
+
+  return interpretation;
+}
+
+/* Love calculator */
+
+function loveCalculator() {
+    prompt("What is your name?");
+    prompt("What is their name?");
+  
+    var loveScore = Math.random() * 100;
+    loveScore = Math.floor(loveScore) + 1;
+    alert("Your love score is " + loveScore + "%");
+  }
+
+/** Leap Year Challenge Exercise */
+
+function isLeap(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return "Leap year.";
+      } else {
+        return "Not leap year.";
+      }
+    } else {
+      return "Leap year.";
+    }
+  } else {
+    return "Not leap year.";
+  }
+}
