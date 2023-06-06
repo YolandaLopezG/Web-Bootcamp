3f007f472f7bc40f15f480d430ef6d8f7cee58ac
YolandaLopezG <ylg.10502@gmail.com>
2023-06-06 00:12:13 -0600

    Manipulating Styles with jQuery

--- JavaScript/JQuery/index.js
+++ JavaScript/JQuery/index.js
@@ -1,1 +1,15 @@
-$("h1").css("color", "red");
+
+// Add css with jquery
+//$("h1").css("color", "red");
+
+// Add css class with jQuery
+//$("h1").addClass("big-title");
+
+// Remove css class with jQuery
+//$("h1").removeClass("big-title");
+
+// Add multiple css classes with jQuery
+$("h1").addClass("big-title margin");
+
+// Ask if has a class
+$("h1").hasClass("big-title");
