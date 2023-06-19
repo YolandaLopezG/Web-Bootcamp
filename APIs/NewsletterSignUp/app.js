16f14eaa3e9400a62470fa664413ce4b81a065a5
YolandaLopezG <ylg.10502@gmail.com>
2023-06-19 15:11:30 -0600

    Send data to mailchimp server

--- APIs/NewsletterSignUp/app.js
+++ APIs/NewsletterSignUp/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+
 
 const app = express();
 
@@ -13,12 +15,44 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-    var name = req.body.firstName;
-    var lastName = req.body.lastName;
-    var email = req.body.email;
-    console.log(name + lastName + email);
+  const name = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: name,
+          LNAME: lastName,
+        },
+      }
+    ]
+  };
+  var jsonData = JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/77ca01fb77";
+  const options = {
+    method: "POST",
+    auth: "Yolanda:82381c87899196079752653682ed629c-us21",
+  };
+
+  const requests = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
   });
+
+  requests.write(jsonData);
+  requests.end();
+});
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
+
+//82381c87899196079752653682ed629c-us21
+
+// 77ca01fb77. audience
