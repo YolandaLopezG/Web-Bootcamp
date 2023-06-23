841e6830cf0a7d74d3b4778ffe13d243f5b990b3
YolandaLopezG <ylg.10502@gmail.com>
2023-06-23 12:04:28 -0600

    Add css

--- EJS/ToDoList/app.js
+++ EJS/ToDoList/app.js
@@ -3,22 +3,23 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy food", 'Cook food', 'Eat food'];
-var item = "";
+let items = ["Buy food", 'Cook food', 'Eat food'];
+let item = "";
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get("/", function (req, res) {
-  var today = new Date();
+  let today = new Date();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", { kindOfDay: day, newListItems: items });
 });
