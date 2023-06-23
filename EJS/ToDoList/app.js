4252d25894cca5cc114da34371a20848348b43f6
YolandaLopezG <ylg.10502@gmail.com>
2023-06-23 13:09:09 -0600

    module exports and refactor

--- EJS/ToDoList/app.js
+++ EJS/ToDoList/app.js
@@ -1,34 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy food", "Cook food", "Eat food"];
-let workItems = [];
-
-//let item = "";
+const items = ["Buy food", "Cook food", "Eat food"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
+  const day = date.getDate();
   res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
-  console.log(req.body.list)
+  const item = req.body.newItem;
   if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
@@ -43,7 +32,7 @@ app.get("/work", function (req, res) {
 });
 
 app.get("/about", function (req, res) {
-  res.render('about');
+  res.render("about");
 });
 
 app.listen(3000, function () {
