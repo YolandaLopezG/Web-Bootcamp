11886985f6be9f3b65e2797d9ed65d2057cf4027
YolandaLopezG <ylg.10502@gmail.com>
2023-06-27 10:24:33 -0600

    challenge 17

--- EJS/ejs-challenge/app.js
+++ EJS/ejs-challenge/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const { forEach } = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -21,7 +22,10 @@ app.use(express.static("public"));
 let allPost = [];
 
 app.get("/", function (req, res) {
-  res.render("home", { startingContent: homeStartingContent, arrayPost: allPost });
+  res.render("home", {
+    startingContent: homeStartingContent,
+    arrayPost: allPost,
+  });
 });
 
 app.get("/about", function (req, res) {
@@ -37,7 +41,13 @@ app.get("/compose", function (req, res) {
 });
 
 app.get("/posts/:postName", function (req, res) {
-  console.log(req.params.postName);
+  const requestedTitle = req.params.postName;
+  allPost.forEach(function (post) {
+    const storedTitle = post.title;
+    if (storedTitle === requestedTitle ) {
+      console.log('match')
+    }
+  });
 });
 
 app.post("/compose", function (req, res) {
