d236d493dbc5fb9f3e1b55fec744554a03e9e864
YolandaLopezG <ylg.10502@gmail.com>
2023-06-29 11:01:10 -0600

    mongoose

--- MongoDB/FruitsProject/app.js
+++ MongoDB/FruitsProject/app.js
@@ -1,29 +1,92 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 
-// Replace the placeholder with your Atlas connection string
-const uri = "mongodb://127.0.0.1:27017";
+//Mogoose connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/fruitsDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`CONNECTED TO MONGO!`);
+  })
+  .catch((err) => {
+    console.log(`OH NO! MONGO CONNECTION ERROR!`);
+    console.log(err);
+  });
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri,  {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    }
-);
+//creating fruit schema
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String,
+});
 
-async function run() {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
+//creating model
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+//creating documents (data)
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty solid as a fruit.",
+});
+
+const orange = new Fruit({
+  name: "Orange",
+  rating: 7,
+  review: "Bad memories.",
+});
+
+const kiwi = new Fruit({
+  name: "Kiwi",
+  rating: 5,
+  review: "Too expensive.",
+});
+
+const banana = new Fruit({
+  name: "Banana",
+  rating: 8,
+  review: "Good flavor.",
+});
+
+/*Fruit.insertMany([kiwi, orange, banana])
+  .then(function () {
+    console.log("Succesfully saved all the fruits to fruitsDB");
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+//storing data
+fruit.save();*/
+
+ Fruit.find({})
+  .then(function (fruits) {
+    console.log(fruits);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+
+// ********************************************************************************************
+// EXERCISE
+/*//Creating Person schema
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+//creating person model
+const Person = mongoose.model("Person", personSchema);
+
+//insert data
+const person = new Person({
+  name: "John",
+  age: 37,
+});
+
+person.save();*/
+
+// ********************************************************************************************
