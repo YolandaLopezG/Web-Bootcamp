1dd0b0e63331ea125fbde6ab9586a94ccccfc82f
unknown <ylg.10502@gmail.com>
2023-07-18 22:36:58 -0600

    Level 1 security

--- Auth/Secrets/app.js
+++ Auth/Secrets/app.js
@@ -3,12 +3,33 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/userDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`CONNECTED TO MONGO!`);
+  })
+  .catch((err) => {
+    console.log(`OH NO! MONGO CONNECTION ERROR!`);
+    console.log(err);
+  });
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
 
 app.get("/", function (req, res) {
   res.render("home");
@@ -20,6 +41,31 @@ app.get("/login", function (req, res) {
 
 app.get("/register", function (req, res) {
   res.render("register");
+});
+
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser
+    .save()
+    .then(() => res.render("secrets"))
+    .catch((err) => console.log(err));
+});
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username })
+    .then((foundUser) => {
+      if (foundUser.password === password) {
+        res.render("secrets");
+      }
+    })
+    .catch((err) => console.log(err));
 });
 
 app.listen(3000, function () {
