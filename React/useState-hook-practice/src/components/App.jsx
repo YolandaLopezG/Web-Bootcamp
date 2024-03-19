20142cecae8627571d30b6b04b415413e682ef33
Lopez, Yolanda (Contractor) <yolanda.lopez@effem.com>
2024-03-19 16:36:09 -0600

    use state

---
+++ React/useState-hook-practice/src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App() {
+  const now = new Date().toLocaleTimeString();
+
+  const [time, setTime] = React.useState(now);
+
+  function getTime() {
+    setTime(new Date().toLocaleTimeString());
+  }
+
+  setInterval(getTime, 1000);
+
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={getTime}>Get Time</button>
+    </div>
+  );
+}
+
+export default App;
